b91958157b992626143bf0ab2a3f5b6e0bee346a
Munsif Siddiqui <siddmuns@sheridancollege.ca>
2024-04-02 09:22:47 -0400

    Completed Header and Created Problem Class

---
+++ src/app/content/problem.ts
@@ -0,0 +1,2 @@
+export class Problem {
+}
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
+import {NgClass} from "@angular/common";
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [
+    NgClass
+  ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
+  quizTitle = 'Assignment 3';
 
+  currentDate() {
+    return new Date().toLocaleDateString();
+  }
+
+  quote = "Tough Times Don't Last, Tough People Do"
 }
